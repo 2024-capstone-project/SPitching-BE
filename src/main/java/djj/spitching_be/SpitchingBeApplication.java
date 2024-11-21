dfdbc326b5714875bf8c314a5abfcc387b0f907d
doleebest <sj27419@gmail.com>
2024-11-21 12:51:27 +0900

    init project

---
+++ src/main/java/djj/spitching_be/SpitchingBeApplication.java
@@ -0,0 +1,13 @@
+package djj.spitching_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpitchingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpitchingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/djj/spitching_be/SpitchingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package djj.spitching_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpitchingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
