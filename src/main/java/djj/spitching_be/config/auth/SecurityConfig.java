5c691016944adf2aefc5ea8821699473993e0d7e
Sojeong Lee <sj27419@gmail.com>
2025-02-12 15:01:14 +0900

    fix : 우선 login 코드 제거

--- src/main/java/djj/spitching_be/config/auth/SecurityConfig.java
+++ src/main/java/djj/spitching_be/config/auth/SecurityConfig.java
@@ -19,67 +19,74 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "https://spitching.store",
-                "https://www.spitching.store"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource(){
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:3000",
+//                "https://spitching.store",
+//                "https://www.spitching.store"
+//        ));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/board/**","/pet/**", "/chat/**","/api/**").authenticated()
+//                        .requestMatchers("/", "/login/**", "/oauth2/**", "/health").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2Login(oauth2 -> oauth2
+//                        .loginPage("/login")
+//                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+//                        .successHandler(((request, response, authentication) -> {
+//                            SecurityContextHolder.getContext().setAuthentication(authentication);
+//                            request.getSession().setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+//
+//                            // 프로덕션과 로컬 환경에 따른 리다이렉트 URI 분기
+//                            String redirectUri = request.getServerName().contains("localhost")
+//                                    ? "http://localhost:8080/loginSuccess"
+//                                    : "https://spitching.store/loginSuccess";
+//
+//                            response.sendRedirect(redirectUri);
+//                        }))
+//                )
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout")
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID")
+//                        .permitAll()
+//                        .logoutSuccessHandler((request, response, authentication) -> {
+//                            response.setStatus(HttpStatus.OK.value());
+//                            String origin = request.getServerName().contains("localhost")
+//                                    ? "http://localhost:3000"
+//                                    : "https://spitching.store";
+//                            response.setHeader("Access-Control-Allow-Origin", origin);
+//                            response.setHeader("Access-Control-Allow-Credentials", "true");
+//                            response.getWriter().write("Logout successful");
+//                            response.getWriter().flush();
+//                        })
+//                );
+//
+//        return http.build();
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/board/**","/pet/**", "/chat/**","/api/**").authenticated()
-                        .requestMatchers("/", "/login/**", "/oauth2/**", "/health").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")
-                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .successHandler(((request, response, authentication) -> {
-                            SecurityContextHolder.getContext().setAuthentication(authentication);
-                            request.getSession().setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-
-                            // 프로덕션과 로컬 환경에 따른 리다이렉트 URI 분기
-                            String redirectUri = request.getServerName().contains("localhost")
-                                    ? "http://localhost:8080/loginSuccess"
-                                    : "https://spitching.store/loginSuccess";
-
-                            response.sendRedirect(redirectUri);
-                        }))
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll()
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            response.setStatus(HttpStatus.OK.value());
-                            String origin = request.getServerName().contains("localhost")
-                                    ? "http://localhost:3000"
-                                    : "https://spitching.store";
-                            response.setHeader("Access-Control-Allow-Origin", origin);
-                            response.setHeader("Access-Control-Allow-Credentials", "true");
-                            response.getWriter().write("Logout successful");
-                            response.getWriter().flush();
-                        })
+                        .requestMatchers("/**").permitAll()
                 );
 
         return http.build();
