12453dcd471c1997661726659b8d2d1827a79b76
Sojeong Lee <sj27419@gmail.com>
2025-02-17 22:37:29 +0900

    fix : auth controller 로 session 초기화

--- src/main/java/djj/spitching_be/Controller/AuthController.java
+++ src/main/java/djj/spitching_be/Controller/AuthController.java
@@ -3,25 +3,18 @@ package djj.spitching_be.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";  // login.html을 반환
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return ResponseEntity.ok("Logged out");
     }
-
-    @GetMapping("/loginSuccess")
-    @ResponseBody  // 문자열을 직접 반환하기 위해 추가
-    public String loginSuccess() {
-        return "Login Successful!";
-    }
-
 }
