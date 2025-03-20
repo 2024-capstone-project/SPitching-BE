aca2b28fa0d7dbe8a8e7aa2ef89f5ab1c7fe3be6
Sojeong Lee <sj27419@gmail.com>
2025-03-20 16:37:00 +0900

    feat : q&a chatbot 기능 로컬 테스트까지 완료

--- src/main/java/djj/spitching_be/Controller/QAChatbotController.java
+++ src/main/java/djj/spitching_be/Controller/QAChatbotController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/presentations/{id}/qa-session")
+@RequestMapping("/api/v1/presentations/{presentationId}/qa-session")
 @RequiredArgsConstructor
 public class QAChatbotController {
 
