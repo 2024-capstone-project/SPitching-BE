925f4d385362699fd6c66dfc4da24393b74f8dd2
Sojeong Lee <sj27419@gmail.com>
2025-04-10 16:28:12 +0900

    feat : practice domain and practice request dto

---
+++ src/main/java/djj/spitching_be/Dto/PracticeRequestDto.java
@@ -0,0 +1,14 @@
+package djj.spitching_be.Dto;
+
+import djj.spitching_be.Domain.Practice;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class PracticeRequestDto {
+
+    private Practice.PracticeType practice_type;
+
+    private String duration;
+}
