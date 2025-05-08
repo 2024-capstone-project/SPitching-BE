2c877c9771c034211a86e4567a2c472936bd8637
Sojeong Lee <sj27419@gmail.com>
2025-05-08 17:25:27 +0900

    fix : endpoint url

--- src/main/java/djj/spitching_be/Controller/EyeFeedbackController.java
+++ src/main/java/djj/spitching_be/Controller/EyeFeedbackController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/feedback")
+@RequestMapping("/api/v1/feedback")
 @RequiredArgsConstructor
 public class EyeFeedbackController {
     private final EyeFeedbackService eyeFeedbackService;
